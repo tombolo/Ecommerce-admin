4483a8cd8a9a220c30b98461806821f3e47d777a
tomboloski <ndanumumo93@gmail.com>
2023-07-18 05:25:55 +0300

    day 1

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,10 @@
-import NextAuth, { getServerSession } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
-import clientPromise from '@/lib/mongodb';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import NextAuth, {getServerSession} from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
 
 const adminEmails = [process.env.ADMIN_EMAIL];
+
 
 export const authOptions = {
   secret: process.env.SECRET,
@@ -12,30 +12,14 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' };
-
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null, an error will be displayed advising the user to check their details
-          return null;
-        }
-      },
+      redirect_uri: "http://localhost:3000/api/auth/callback/google"
     }),
   ],
+
+  
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
-    session: ({ session, token, user }) => {
+    session: ({session,token,user}) => {
       if (adminEmails.includes(session?.user?.email)) {
         return session;
       } else {
@@ -47,8 +31,8 @@ export const authOptions = {
 
 export default NextAuth(authOptions);
 
-export async function isAdminRequest(req, res) {
-  const session = await getServerSession(req, res, authOptions);
+export async function isAdminRequest(req,res) {
+  const session = await getServerSession(req,res,authOptions);
   if (!adminEmails.includes(session?.user?.email)) {
     res.status(401);
     res.end();
